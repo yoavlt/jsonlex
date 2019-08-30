e6b455e430d27163e993984d20fad18ed74465e9
Takuma Yoshida <yoa.jmpr.w@gmail.com>
2019-08-30 19:32:18 +0900

    Bump version 0.1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Jsonlex.MixProject do
   def project do
     [
       app: :jsonlex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: @description,
